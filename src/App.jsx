abd9b7d341e2f860118708e1b1877a7ffc13b23e
choiyui <choiyj220220@gmail.com>
2026-06-23 13:51:21 +0900

    feat: content 내용 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,34 @@ import Myheader from "./components/Myheader";
 import Nav from "./components/Nav";
 import MyArticle from "./components/MyArticle";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [subject, setSubject] = useState({
+    title: "프론트엔드 개발자",
+    desc: "기본언어인 html, css, javascript부터 학습합니다.",
+  });
+
+  const [content, setContent] = useState([
+    {
+      id: 1,
+      title: "UI/UX 개발",
+      desc: "사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현",
+    },
+    {
+      id: 2,
+      title: "재사용이 가능한 UI 개발",
+      desc: "컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능",
+    },
+    {
+      id: 3,
+      title: "애니메이션 구현",
+      desc: "상태 변화에 따른 자연스럽고 동적인 화면 효과 구현",
+    },
+  ]);
   return (
     <>
-      <Myheader />
+      <Myheader title={subject.title} desc={subject.desc} />
       <Nav />
       <MyArticle
         title="UI/UX 개발"
--- src/components/Myheader.jsx
+++ src/components/Myheader.jsx
@@ -1,8 +1,8 @@
-function Myheader() {
+function Myheader({ title, desc }) {
   return (
     <header>
-      <h1 className="logo">프론트엔드 개발자</h1>
-      <p>기본언어인 html, css, javascript부터 학습합니다.</p>
+      <h1 className="logo">{title}</h1>
+      <p>{desc}</p>
     </header>
   );
 }
