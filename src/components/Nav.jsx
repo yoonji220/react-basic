719a9c508d0b69392b33e02fcdc87087e76e933d
choiyui <choiyj220220@gmail.com>
2026-06-23 15:47:12 +0900

    feat: 클릭시 id 반영

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,31 @@
-function Nav({ data }) {
+function Nav({ data, onChangeMode }) {
   console.log("Nav render");
   const lists = data.map(d => (
+    //   <li key={d.id}>
+    //     <a
+    //       href=""
+    //       onClick={e => {
+    //         e.preventDefault();
+    //         onChangeMode(d.id);
+    //       }}
+    //     >
+    //       {d.title}
+    //     </a>
+    //   </li>
+    // ));
     <li key={d.id}>
-      <a href="">{d.title}</a>
+      <a
+        href={`/${d.id}`}
+        data-id={d.id}
+        onClick={e => {
+          // console.log(e.target.dataset.id);
+
+          e.preventDefault();
+          onChangeMode(Number(e.target.dataset.id));
+        }}
+      >
+        {d.title}
+      </a>
     </li>
   ));
   return (
