88d2fefe744f6e7a30063df0f10e60408cb2ef09
choiyui <choiyj220220@gmail.com>
2026-06-24 14:13:58 +0900

    feat: update article update

--- src/components/UpdateArticle.jsx
+++ src/components/UpdateArticle.jsx
@@ -1,5 +1,15 @@
-function UpdateArticle({ onSubmit }) {
+import { useState } from "react";
+
+function UpdateArticle({ title, desc, onSubmit }) {
   console.log("UpdateArticle render");
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDesc, setNewDesc] = useState(desc);
+  const handleTitleChange = e => {
+    setNewTitle(e.target.value);
+  };
+  const handleDescChange = e => {
+    setNewDesc(e.target.value);
+  };
   return (
     <>
       <h2>Update Article</h2>
@@ -12,11 +22,17 @@ function UpdateArticle({ onSubmit }) {
       >
         <div>
           <label htmlFor="title">title</label>
-          <input type="text" name="title" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={newTitle}
+            onChange={handleTitleChange}
+          />
         </div>
         <div>
           <label htmlFor="desc">desc</label>
-          <textarea name="desc" id="desc"></textarea>
+          <textarea name="desc" id="desc" value={newDesc} onChange={handleDescChange}></textarea>
         </div>
         <button>Submit</button>
       </form>
