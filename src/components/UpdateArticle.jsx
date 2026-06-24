785a0468f538e4181461dbbc95e010686174d452
choiyui <choiyj220220@gmail.com>
2026-06-24 11:47:02 +0900

    feat : 수정, 삭제 버튼 추가

---
+++ src/components/UpdateArticle.jsx
@@ -0,0 +1,26 @@
+function UpdateArticle({ onSubmit }) {
+  console.log("UpdateArticle render");
+  return (
+    <>
+      <h2>Update Article</h2>
+      <form
+        action=""
+        onSubmit={e => {
+          e.preventDefault();
+          onSubmit(e.target.title.value, e.target.desc.value);
+        }}
+      >
+        <div>
+          <label htmlFor="title">title</label>
+          <input type="text" name="title" id="title" />
+        </div>
+        <div>
+          <label htmlFor="desc">desc</label>
+          <textarea name="desc" id="desc"></textarea>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
+export default UpdateArticle;
